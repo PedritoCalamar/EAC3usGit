dd45ca16ab757f0dc496e9caf1e5ee023f1f8bc4
Pedro Navarro <pnavarrom2011@gmail.com>
2019-12-08 14:35:36 +0100

    Modificar les classes Persona i Principal

--- EAC3usGit/src/Projecte/Persona.java
+++ EAC3usGit/src/Projecte/Persona.java
@@ -5,12 +5,24 @@ public class Persona {
 	int id;
 	String nom;
 	String cognoms;
-		
+	String email;
+	String carrec;
+	
 	public Persona(int id, String nom, String cognoms) {
 		super();
 		this.id = id;
 		this.nom = nom;
 		this.cognoms = cognoms;
+		
+	}
+	
+	public Persona(int id, String nom, String cognoms,String email, String carrec) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.cognoms = cognoms;
+		this.email = email;
+		this.carrec = carrec;
 		
 	}
 
--- EAC3usGit/src/Projecte/Principal.java
+++ EAC3usGit/src/Projecte/Principal.java
@@ -11,9 +11,10 @@ public class Principal {
 		
 		Persona persona1 = new Persona(1,"Josep","Canals Ponts");
 		Persona persona2 = new Persona(2,"Maria","Lopez Garrido");
-		
+		Persona persona3 = new Persona(3,"Joana","Avilés Serra","javiles@gmil.com","Analista");
 		llistaP.add(persona1);
 		llistaP.add(persona2);
+		llistaP.add(persona3);
 				
 		for (Persona p : llistaP) {
 			System.out.println(p.id);
